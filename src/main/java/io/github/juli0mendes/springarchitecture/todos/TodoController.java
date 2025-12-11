6d6bd4aba39ffda7345d231d0abfb45c38beec2a
juli0mendes <jmendes@live.com>
2025-12-10 22:12:19 -0300

    feat: implement Todo entity with controller, service, and repository

---
+++ src/main/java/io/github/juli0mendes/springarchitecture/todos/TodoController.java
@@ -0,0 +1,38 @@
+package io.github.juli0mendes.springarchitecture.todos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService service;
+
+    public TodoController(TodoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public TodoEntity salvar(@RequestBody TodoEntity entity) {
+        return this.service.save(entity);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") Integer id, @RequestBody TodoEntity entity) {
+        entity.setId(id);
+        this.service.update(entity);
+    }
+
+    @GetMapping("{id}")
+    public TodoEntity getById(@PathVariable("id") Integer id) {
+        return this.service.findById(id);
+    }
+}
