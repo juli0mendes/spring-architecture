c6eeee5a62bf392f53b979cf9adf48b91d56fad3
juli0mendes <jmendes@live.com>
2025-12-11 21:32:24 -0300

    feat: add MailSender and TodoValidator components with validation logic for unique descriptions

--- src/main/java/io/github/juli0mendes/springarchitecture/todos/TodoController.java
+++ src/main/java/io/github/juli0mendes/springarchitecture/todos/TodoController.java
@@ -1,5 +1,6 @@
 package io.github.juli0mendes.springarchitecture.todos;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/todos")
@@ -20,7 +22,12 @@ public class TodoController {
 
     @PostMapping
     public TodoEntity salvar(@RequestBody TodoEntity entity) {
-        return this.service.save(entity);
+        try {
+            return this.service.save(entity);
+        } catch (IllegalArgumentException e) {
+            var errorMessage = "Error saving todo: " + e.getMessage();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
+        }
     }
 
     @PutMapping("{id}")
--- src/main/java/io/github/juli0mendes/springarchitecture/todos/TodoService.java
+++ src/main/java/io/github/juli0mendes/springarchitecture/todos/TodoService.java
@@ -6,17 +6,26 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 
     private final TodoRepository repository;
+    private final TodoValidator validator;
+    private final MailSender mailSender;
 
-    public TodoService(final TodoRepository repository) {
+    public TodoService(final TodoRepository repository,
+                       final TodoValidator validator,
+                       final MailSender mailSender) {
         this.repository = repository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
 
     public TodoEntity save(TodoEntity entity) {
+        this.validator.validate(entity);
         return this.repository.save(entity);
     }
 
     public void update(TodoEntity entity) {
         this.repository.save(entity);
+        var status = entity.getDone() == Boolean.TRUE ? "done" : "not done";
+        this.mailSender.send("Todo updated: " + entity.getDescription() + " was " + status);
     }
 
     public TodoEntity findById(Integer id) {
